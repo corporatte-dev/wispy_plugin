221c830d8cfc1818a4f5551f65a7acbd2eee65b9
Ryan <rpate5282@hotmail.com>
2022-08-05 14:17:23 -0600

    Fixed rendering when window scale changes (temporary fix)

---
+++ src/ServerStorage/Wispy/Library/Defer.lua
@@ -0,0 +1,56 @@
+local Defer = {} 
+Defer.__index = Defer
+
+--[=[
+    |> Defered Callback System
+
+    defer may not be the best name lol.
+
+    This system will call the function only if no updates were given in a span of X seconds.
+]=]
+
+function Defer.new(Callback: (any) -> any, Timeout: number)
+    local New = {
+        Count = 0,
+        Callback = Callback,
+        Timeout = Timeout * 10,
+        Reset = false,
+        Alive = true
+    }
+
+    task.spawn(function()
+        while New.Alive do
+            if New.Reset then
+                New.Count = 0
+                New.Reset = false
+            end
+
+            task.wait(0.1)
+            if New.Count <= New.Timeout then
+                New.Count += 1
+                print(New.Count)
+            end
+
+
+            if New.Count == New.Timeout then
+                
+                Callback()
+            end
+        end
+    end)
+
+
+    return setmetatable(New, Defer)
+end
+
+function Defer:Call()
+    self.Reset = true
+end
+
+function Defer:Clean()
+    self.Alive = false
+    self = nil
+end
+
+
+return Defer
