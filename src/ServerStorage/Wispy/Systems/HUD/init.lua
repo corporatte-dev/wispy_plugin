9a92359fb9439a32131bc0f7cf9a4f4938a8d3b0
Ryan <rpate5282@hotmail.com>
2022-08-05 10:17:22 -0600

    Added comments to HUD System

--- src/ServerStorage/Wispy/Systems/HUD/init.lua
+++ src/ServerStorage/Wispy/Systems/HUD/init.lua
@@ -2,4 +2,10 @@ local Types = require(script.Parent.Parent.Types)
 local HUD = {} :: Types.HUD
 HUD.NoMount = true
 
+--[=[
+    |> HUD System
+
+    This should handle UI on the viewport such as notifications.
+]=]
+
 return HUD
